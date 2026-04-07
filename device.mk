86c73ce22a5c830df08f64846b5acabc1c9d68ea
Mashopy <eliasgheeraert@gmail.com>
2026-04-07 10:57:08 +0200

    salaa: Rename memtrack service to the new impl
    Change-Id: I8dc85520aa16b0e5b687fa71f3b23d9b5a9807c8
    Signed-off-by: Debayan Kar <kardebayan3@gmail.com>

--- device.mk
+++ device.mk
@@ -77,7 +77,7 @@ PRODUCT_PACKAGES += \
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.2-service \
-    android.hardware.memtrack-service.mediatek-mali
+    android.hardware.memtrack-service.mediatek
 
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
